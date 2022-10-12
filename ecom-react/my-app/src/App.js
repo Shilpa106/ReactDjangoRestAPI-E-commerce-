2217d0bf6850c54d18b4e201a7fa590c0e1976d6
Shilpa106 <yshilpa234@gmail.com>
2022-10-12 19:36:19 +0530

    react app

---
+++ ecom-react/my-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+import {Container} from 'react-bootstrap'
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main>
+      <Container><h1>Welcome to my shop</h1></Container>
+      </main>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
